703fc29e72d3572322261d38642f6d168df28d45
Aaditi <aadi2007.aaditi@gmail.com>
2020-09-08 22:07:52 -0700

    add entire html

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 //const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 //const [name, github] = profileDataArgs;
@@ -144,5 +144,12 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+   
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+       console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
